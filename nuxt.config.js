0d32b025c645ae71c259d5f01c62d2d79e6ad8c4
EKShin <eks012@gmail.com>
2018-04-19 22:52:59 +0900

    INIT: 레이아웃

---
+++ nuxt.config.js
@@ -0,0 +1,40 @@
+module.exports = {
+  /*
+  ** Build configuration
+  */
+  build: {
+    postcss: {
+      plugins: {
+        'postcss-custom-properties': false
+      }
+    }
+  },
+  /*
+  ** Headers
+  ** Common headers are already provided by @nuxtjs/pwa preset
+  */
+  head: {
+    link: [
+      { rel: 'stylesheet', href: 'https://cdn.jsdelivr.net/gh/Joungkyun/font-d2coding@1.31.0/d2coding.css' }
+    ]
+  },
+  /*
+  ** Customize the progress-bar color
+  */
+  loading: { color: '#3B8070' },
+  /*
+  ** Customize app manifest
+  */
+  manifest: {
+    theme_color: '#3B8070'
+  },
+  /*
+  ** Modules
+  */
+  modules: [
+    '@nuxtjs/pwa',
+    '@nuxtjs/toast',
+    '@nuxtjs/axios',
+    '@nuxtjs/bulma'
+  ]
+}
