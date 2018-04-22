20f7c3a32e712afd54d63ba7e55df3922ecddcd6
khzero <mo02@outlook.kr>
2018-04-23 00:16:44 +0900

    메인 particles 추가

---
+++ components/Particles.vue
@@ -0,0 +1,28 @@
+<template>
+      <vue-particles
+        color="#ffffff"
+        :particleOpacity="0.7"
+        :particlesNumber="80"
+        shapeType="circle"
+        :particleSize="4"
+        linesColor="#ffffff"
+        :linesWidth="1"
+        :lineLinked="true"
+        :lineOpacity="0.4"
+        :linesDistance="150"
+        :moveSpeed="3"
+        :hoverEffect="true"
+        hoverMode="grab"
+        :clickEffect="true"
+        clickMode="push"
+      >
+      </vue-particles>
+</template>
+<style scoped>
+  #particles-js {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+  }
+</style>
