b293509ee173b156c83a87cfcffe68c8ff0684ae
EKShin <eks012@gmail.com>
2018-05-22 00:23:55 +0900

    members에 섹션값 추가

--- components/intro/Members.vue
+++ components/intro/Members.vue
@@ -1,15 +1,17 @@
 <template>
-  <div class="tile is-ancestor">
-    <div class="tile is-parent">
-      <article class="tile is-child notification is-white">
-        <div class="content member-list">
-          <figure class="image is-128x128" v-for="(value,key) in memberList" :key="key">
-            <img class="profile-image" :src="value.img" />
-            <figcaption>{{value.name}}</figcaption>
-            <figcaption v-if="value.motto">"{{value.motto}}"</figcaption>
-          </figure>
-        </div>
-      </article>
+  <div class="section">
+    <div class="tile is-ancestor">
+      <div class="tile is-parent">
+        <article class="tile is-child notification is-white">
+          <div class="content member-list">
+            <figure class="image is-128x128" v-for="(value,key) in memberList" :key="key">
+              <img class="profile-image" :src="value.img" />
+              <figcaption>{{value.name}}</figcaption>
+              <figcaption v-if="value.motto">"{{value.motto}}"</figcaption>
+            </figure>
+          </div>
+        </article>
+      </div>
     </div>
   </div>
 </template>
