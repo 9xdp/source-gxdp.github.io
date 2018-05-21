6b519d41ec828affa21bf1e8ca47583ce8a32d85
EKShin <eks012@gmail.com>
2018-05-21 23:03:38 +0900

    about 문구 수정

--- components/intro/About.vue
+++ components/intro/About.vue
@@ -1,19 +1,19 @@
 <template>
   <div class="intro section">
     <h2 class="title">About 9xdp</h2><br />
-    <!-- grid images -->
+
     <div class="columns">
       <div class="column">
          <div class="card is-transparency">
             <div class="card-image">
               <figure class="image is-96x96">
-                <img class="is-rounded" src="~assets/about/96x96.png" alt="Placeholder image">
+                <img class="is-rounded" src="~assets/about/circle_meetup.png" alt="정기 모임">
                 <figcaption></figcaption>
               </figure>
             </div>
             <div class="card-content">
               <h4 class="has-text-primary is-size-6 has-text-weight-semibold">정기 모임</h4>
-              <p class="is-size-7">퇴근 후 컴퓨터 앞에 앉게 해줄 수 있는 건 내가 만들고 싶었던 나만의 프로젝트입니다. 한 달에 적어도 한 번씩 서로의 프로젝트를 위한 밋업을 가집니다.</p>
+              <p class="is-size-6">퇴근 후 컴퓨터 앞에 앉게 해줄 수 있는 건 내가 만들고 싶었던 나만의 프로젝트입니다. 한 달에 적어도 한 번씩 서로의 프로젝트를 위한 밋업을 가집니다.</p>
             </div>
         </div>
 
@@ -22,13 +22,13 @@
          <div class="card is-transparency">
           <div class="card-image">
             <figure class="image is-96x96">
-              <img class="is-rounded" src="~assets/about/96x96.png" alt="Placeholder image">
+              <img class="is-rounded" src="~assets/about/circle_overtime.png" alt="야근은 이제 그만">
               <figcaption></figcaption>
             </figure>
           </div>
           <div class="card-content">
-            <h4 class="has-text-primary is-size-6">2</h4>
-            <p class="is-size-7">가나다라마바사</p>
+            <h4 class="has-text-primary is-size-6 has-text-weight-semibold">너와 함께한 시간 모두 눈부셨다</h4>
+            <p class="is-size-6">하고 싶은 프로젝트를 또래 개발자들과 함께 시작해보세요. 프로젝트 커리어, 오픈 소스 사용 경험을 늘려보세요.</p>
           </div>
         </div>
       </div>
